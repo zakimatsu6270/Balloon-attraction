fef5c384cd917b2b55d91caedb28c40c3c8499fa
LD10-KAISHI-MAT\kaishi <kaishi627@i.softbank.jp>
2018-04-29 08:06:46 +0900

    スタート

---
+++ OriginalGame/Assets/Wind.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class Wind : MonoBehaviour {
+    public float coefficient;   // 空気抵抗係数
+    public Vector3 velocity;    // 風速
+ 
+	void OnTriggerStay(Collider col) {
+        if ( col.GetComponent<Rigidbody>() == null ) {
+            return;
+        }
+ 
+        // 相対速度計算
+        var relativeVelocity = velocity - col.rigidbody.velocity;
+ 
+	    // 空気抵抗を与える
+        col.rigidbody.AddForce(coefficient * relativeVelocity);
+	}
+}
