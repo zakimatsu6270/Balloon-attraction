785ca04953630863d8cab025bfbd04fb9f1cb48e
LD10-KAISHI-MAT\kaishi <kaishi627@i.softbank.jp>
2018-04-30 18:12:23 +0900

    スタート

--- OriginalGame/Assets/Wind.cs
+++ OriginalGame/Assets/Wind.cs
@@ -2,8 +2,8 @@
 using System.Collections;
  
 public class Wind : MonoBehaviour {
-    public float coefficient;   // 空気抵抗係数
-    public Vector3 velocity;    // 風速
+    public float coefficient = 0;   // 空気抵抗係数
+    public Vector3 velocity = new Vector3(0,5,5);    // 風速
  
 	void OnTriggerStay(Collider col) {
         if ( col.GetComponent<Rigidbody>() == null ) {
@@ -11,9 +11,9 @@ public class Wind : MonoBehaviour {
         }
  
         // 相対速度計算
-        var relativeVelocity = velocity - col.rigidbody.velocity;
+        var relativeVelocity = velocity - col.GetComponent<Rigidbody>().velocity;
  
 	    // 空気抵抗を与える
-        col.rigidbody.AddForce(coefficient * relativeVelocity);
+        col.GetComponent<Rigidbody>().AddForce(coefficient * relativeVelocity);
 	}
 }
---
+++ OriginalGame/Assets/Wind1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().velocity = new Vector3(0,6,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
